7866ecf90d5199c0d1b80bad045345af014b0589
Ken Toole <kdt33@cornell.edu>
2017-03-19 08:03:26 -0700

    schema and scaffolding clean up

--- db/schema.rb
+++ db/schema.rb
@@ -21,11 +21,6 @@ ActiveRecord::Schema.define(version: 20170315193921) do
     t.index ["user_id"], name: "index_clients_on_user_id", using: :btree
   end
 
-  create_table "homes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
